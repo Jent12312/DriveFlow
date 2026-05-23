d0883c6c0eb109d5b7ee650fddf559086ab5c00e
Jent12312 <108217503+Jent12312@users.noreply.github.com>
2026-05-23 18:01:43 +0300

    Create vite.config.ts

---
+++ driveflow-frontend/vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
+});
